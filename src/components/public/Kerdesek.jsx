414a04360427b450dfc2278367154665f0fab6f4
csefikatalin <csefi.katalin@gmail.com>
2025-12-04 13:55:14 +0100

    quiz szintjének  választása

--- src/components/public/Kerdesek.jsx
+++ src/components/public/Kerdesek.jsx
@@ -1,25 +1,52 @@
 import React, { useContext } from "react";
 import Kerdes from "./Kerdes";
 import { KerdesekContext } from "../../contexts/KerdesekContext";
+import { useState } from "react";
+import { useMemo } from "react";
 
 export default function Kerdesek() {
-  const { kerdesekLista,loading, pont } = useContext(KerdesekContext);
-   if (loading) {
+  const [difficulty, setDifficulty] = useState("medium");
+  const { kerdesekLista, loading, pont } = useContext(KerdesekContext);
+  const szurtLista = useMemo(() => {
+    return kerdesekLista.filter((k) => k.difficulty === difficulty).slice(0,10);
+  }, [kerdesekLista, difficulty]);
+
+  if (loading) {
     // Betöltés alatt ezt jeleníti meg
     return <div>Betöltés folyamatban...</div>;
   }
-  if (!kerdesekLista || kerdesekLista.length === 0) {
+  if (!szurtLista || szurtLista.length === 0) {
     // Ha nincs adat
     return <div>Nincsenek kérdések.</div>;
   }
-return (
+
+  function handleChange(e) {
+    setDifficulty(e.target.value);
+  }
+  return (
     <div className="">
-      <h2>Kérdések</h2> 
+      <h2>Kérdések</h2>
+      <div className="mb-3">
+        <label htmlFor="difficulty" className="form-label">
+          Válassz nehézségi szintet!
+        </label>
+        <select
+          id="difficulty"
+          className="form-select"
+          value={difficulty}
+          onChange={handleChange}
+        >
+          <option value="easy">Könnyű</option>
+          <option value="medium">Közepes</option>
+          <option value="hard">Nehéz</option>
+        </select>
+      </div>
+
       <p>Elért pontszám: {pont}</p>
       <div className="card">
-      {kerdesekLista.map((kerdes) => (
-        <Kerdes key={kerdes.id} kerdes={kerdes} />
-      ))}
+        {szurtLista.map((kerdes) => (
+          <Kerdes key={kerdes.id} kerdes={kerdes} />
+        ))}
       </div>
     </div>
   );
--- src/components/public/TopLista.jsx
+++ src/components/public/TopLista.jsx
@@ -5,12 +5,21 @@ import { useEffect } from "react";
 
 export default function TopLista() {
   const { loading, userLista, getUser } = useContext(UserContext);
-    useEffect(() => {
+  useEffect(() => {
     getUser();
   }, []);
+
+  if (loading) {
+    // Betöltés alatt ezt jeleníti meg
+    return <div>Betöltés folyamatban...</div>;
+  }
+  if (!userLista || userLista.length === 0) {
+    // Ha nincs adat
+    return <div>Még senki nem töltötte ki a quizt.</div>;
+  }
   const top10 = [...userLista]
-  .sort((a, b) => b.point - a.point) // csökkenő sorrend pont szerint
-  .slice(0, 10); // első 10 elem
+    .sort((a, b) => b.point - a.point) // csökkenő sorrend pont szerint
+    .slice(0, 10); // első 10 elem
   return (
     <>
       <h2>Toplista</h2>
@@ -18,7 +27,9 @@ export default function TopLista() {
         {top10.map((user, index) => {
           return (
             <li key={user.id} className="list-group-item row">
-              <span className="col-2 ">{index+1}. <strong>{user.name}</strong></span>
+              <span className="col-2 ">
+                {index + 1}. <strong>{user.name}</strong>
+              </span>
               <span className="col-2">{user.point} pont</span>
             </li>
           );
